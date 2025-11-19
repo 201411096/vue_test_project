a3e46cc19c6f2cc7d627d1b566e3a42c698f08b1
201411096 <korea5781@gmail.com>
2025-11-19 16:25:13 +0900

    .

--- src/vue/nextTick.vue
+++ src/vue/nextTick.vue
@@ -6,6 +6,8 @@ export default {
   data() {
     return {
       counter: 0,
+      showFg_content01:false,
+      showFg_content02:false,
     };
   },
   methods: {
@@ -13,14 +15,25 @@ export default {
       this.counter++;
       console.log("counter : ", document.getElementById("counter").textContent);
       await nextTick();
-      console.log(
-        "counter after nextTick() : ",
-        document.getElementById("counter").textContent
-      );
-      console.log(
-        "--------------------------------------------------------------------------------"
-      );
+      console.log("counter after nextTick() : ", document.getElementById("counter").textContent);
+      console.log("--------------------------------------------------------------------------------");
     },
+
+    async toggleConent01(){
+      console.log('this.$refs.content01 : ', this.$refs.content01);
+      this.showFg_content01 = !this.showFg_content01;
+      await nextTick();
+      console.log('this.$refs.content01 after nextTick() : ', this.$refs.content01);
+      console.log("--------------------------------------------------------------------------------");
+    },
+
+    async toggleConent02(){
+      console.log('this.$refs.content02 : ', this.$refs.content02);
+      this.showFg_content02 = !this.showFg_content02;
+      await nextTick();
+      console.log('this.$refs.content02 after nextTick() : ', this.$refs.content02);
+      console.log("--------------------------------------------------------------------------------");
+    }
   },
 };
 </script>
@@ -36,6 +49,28 @@ export default {
     <div>
       <button @click="addCounter">addCounter</button>
     </div>
+
+    <hr></hr>
+
+    <div>
+      <button @click="toggleConent01">toggleConent01</button>
+    </div>
+    <div v-if="showFg_content01" ref="content01">
+      content01
+      <span></span>
+    </div>
+
+    <hr></hr>
+
+    <div>
+      <button @click="toggleConent02">toggleConent02</button>
+    </div>
+    <div v-show="showFg_content02" ref="content02">
+      content02
+      <span></span>
+    </div>
+
+    
   </header>
 </template>
 
