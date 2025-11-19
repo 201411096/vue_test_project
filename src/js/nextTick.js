d5276a6f7d38e8734c044d5f598f0d1667bb0f9c
201411096 <korea5781@gmail.com>
2025-11-19 16:04:18 +0900

    .

---
+++ src/js/nextTick.js
@@ -0,0 +1,6 @@
+// import "/src/assets/main.css";
+
+import { createApp } from "vue";
+import nextTick from "/src/vue/nextTick.vue";
+
+createApp(nextTick).mount("#app");
---
+++ src/vue/nextTick.vue
@@ -0,0 +1,42 @@
+<script>
+import { nextTick } from "vue";
+
+export default {
+  components: {},
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    async addCounter() {
+      this.counter++;
+      console.log("counter : ", document.getElementById("counter").textContent);
+      await nextTick();
+      console.log(
+        "counter after nextTick() : ",
+        document.getElementById("counter").textContent
+      );
+      console.log(
+        "--------------------------------------------------------------------------------"
+      );
+    },
+  },
+};
+</script>
+
+<template>
+  <header>
+    <div>nextTick test page..</div>
+
+    <div>
+      <span>counter : </span>
+      <span id="counter">{{ counter }} </span>
+    </div>
+    <div>
+      <button @click="addCounter">addCounter</button>
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
